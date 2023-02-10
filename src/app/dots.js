91d2e5000bc0d674cdf25943c41e21d50efbca6d
billy <billymylesberkouwer@gmail.com>
2023-02-10 12:51:10 +0000

    axis-and-improved-styline

--- src/app/dots.js
+++ src/app/dots.js
@@ -1,7 +1,8 @@
 export default function Dots({mouseX, mouseY, opacity}) {
     return (
         <div className="mouseObjectContainer" style={{opacity: opacity, top: mouseY, left: mouseX}} >
-            <div className="mouseObject" style={{backgroundColor: 'red'}}></div>
+            <div className="mouseObjectBackground"></div>
+            <div className="mouseObject" style={{backgroundColor: 'rgba(255,255,255,0.1)'}}></div>
             <p className="mouseText">X: {mouseX} Y: {mouseY}</p>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -5,65 +5,125 @@ import { Plane } from "components/3d/Plane";
 import { Torus } from "components/3d/Torus";
 import useClickCount from "hooks/useClickCount";
 import useMousePosition from "hooks/useMousePosition";
-import usePageWidth from "hooks/usePageSize";
+import usepageSize from "hooks/usePageSize";
 import isMobile from "is-mobile";
 import { createElement, useEffect, useState } from "react";
 import Dots from "./dots";
 
 export default function HomePage(props) {
-  const pageWidth = usePageWidth();
+  const pageSize = usepageSize();
   const mousePosition = useMousePosition();
   const clickCount = useClickCount();
-   const [dots, setDots] = useState([]);
-   const [mousePositions, setMousePositions] = useState([]);
+  const [dots, setDots] = useState([]);
+  const [mousePositions, setMousePositions] = useState([]);
+  const [isMobileClient, setIsMobileClient] = useState(false);
+  const [maxScreen, setMaxScreen] = useState({width: 0, height: 0});
+  const [scaleIncrements, setScaleIncrements] = useState([])
 
   useEffect(() => {
-      const mousePos = {x: mousePosition.x, y: mousePosition.y};
-      setMousePositions(prev => [...prev.slice(prev.length-300), mousePos]);
+    setIsMobileClient(isMobile());
+    setMaxScreen({width: window.innerWidth, height: window.innerHeight})
+  }, []);
+
+  useEffect(() => {
+    const widthInc = Math.round(pageSize.x / 10);
+    const heightInc = Math.round(pageSize.y / 10);
+    const increments = [];
+    for (let i = 0; i < 10; i++) {
+      increments.push({
+        width: widthInc * i,
+        height: heightInc * i,
+      })
+    }
+    setScaleIncrements(increments)
+  }, [pageSize])
+
+  useEffect(() => {
+    const mousePos = { x: mousePosition.x, y: mousePosition.y };
+    setMousePositions((prev) => [...prev.slice(prev.length - 300), mousePos]);
   }, [clickCount]);
-  
-  if (isMobile()) {
+
+  if (isMobileClient) {
     return (
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw', textAlign: 'center'}}>
-        <Canvas style={{position: 'absolute', height: '100vh', width: '100vw', top: 0}} >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+          textAlign: "center",
+        }}
+      >
+        <Canvas
+          style={{
+            position: "absolute",
+            height: "100vh",
+            width: "100vw",
+            top: 0,
+          }}
+        >
           <Torus />
           <Plane />
         </Canvas>
-        <h2 style={{maxWidth: '200px'}}>
-          visit this site on desktop
-        </h2>
+        <h3 style={{ maxWidth: "200px" }}>visit this site on desktop</h3>
       </div>
-    )
-  } else {return (
-    <div id="container">
-      <Canvas style={{position: 'absolute', height: '100vh', width: '100vw', top: 0}} >
-        <fog attach="fog" color="black" near={1} far={10} />
-        <Torus />
-        <Plane />
-      </Canvas>
-      <p>
-        {pageWidth.x} {pageWidth.y}
-      </p>
-      {mousePosition.x && mousePosition.y ? (
-        <div
-          className="mouseObjectContainer"
-          style={{ 
-            top: mousePosition.y ,
-            left: mousePosition.x ,
+    );
+  } else {
+    return (
+      <div id="container" style={{maxWidth: maxScreen.width + 'px', maxHeight: maxScreen.height + 'px', overflow: 'hidden', width: '100%', height: '100vh'}}>
+        <Canvas
+          style={{
+            position: "absolute",
+            height: "100vh",
+            width: "100vw",
+            top: 0,
           }}
         >
-          <div className="mouseObject" style={{border: 'solid white 1px'}}></div>
-          <p className="mouseText">X: {mousePosition.x} Y: {mousePosition.y}</p>
-        </div>
-      ) : null}
-      {mousePositions.map((el, i) => {
-        if (mousePositions.length - i <=300) {
+          <fog attach="fog" color="black" near={1} far={10} />
+          <Torus />
+          <Plane />
+        </Canvas>
+        {mousePosition.x && mousePosition.y ? (
+          <div
+            className="mouseObjectContainer"
+            style={{
+              top: mousePosition.y,
+              left: mousePosition.x,
+            }}
+          >
+            <div
+              className="mouseObject"
+              style={{ border: "solid white 1px" }}
+            ></div>
+            <p className="mouseText">
+              X: {mousePosition.x} Y: {mousePosition.y}
+            </p>
+          </div>
+        ) : null}
+        <div className="axis" />
+        {scaleIncrements.map((el, i) =>
+          <p className="axisNumber axisNumberX" style={{left: el.width}}>{el.width}</p>
+        )}
+        {scaleIncrements.map((el, i) =>
+        <>
+          <p className="axisNumber axisNumberY" style={{top: el.height}}>{el.height}</p>
+          {console.log(el)}
+        </>
+        )}
+        {mousePositions.map((el, i) => {
+          if (mousePositions.length - i <= 300) {
             return (
-              <Dots key={'dot ' + i} mouseX={el.x} mouseY={el.y} opacity={i/mousePositions.length} />  
-            )
+              <Dots
+                key={"dot " + i}
+                mouseX={el.x}
+                mouseY={el.y}
+                opacity={i / mousePositions.length}
+              />
+            );
           }
-        }
-      )}
-    </div>
-  );}
+        })}
+      </div>
+    );
+  }
 }
