07c763ad8461d6e4b61f2c8c2da6d631f8b51b98
billy <billymylesberkouwer@gmail.com>
2023-02-10 13:58:50 +0000

    name-change

--- src/app/head.js
+++ src/app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Experiment In Digital Space</title>
+      <title>Study of Digital Space</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="An experiment in digital space by Billy Myles-Berkouwer" />
       <link rel="icon" href="/favicon.jpg" />
